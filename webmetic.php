34f627be68eaec588c1d09995faca16980541d4b
Yannik Süß <yanniksuess@gmail.com>
2025-06-24 11:57:41 +0200

    Update author information in plugin metadata

--- webmetic.php
+++ webmetic.php
@@ -6,8 +6,8 @@
  * Version:           1.0.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Yannik Süß
- * Author URI:        https://github.com/yanniksuess
+ * Author:            Webmetic GmbH
+ * Author URI:        https://webmetic.de
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       webmetic
