29f73773687ccd152a4d9dbbbf456a94e4bbdf13
Yannik Süß <yanniksuess@gmail.com>
2025-06-24 11:57:11 +0200

    Added Author

--- webmetic.php
+++ webmetic.php
@@ -6,6 +6,8 @@
  * Version:           1.0.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
+ * Author:            Yannik Süß
+ * Author URI:        https://github.com/yanniksuess
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       webmetic
